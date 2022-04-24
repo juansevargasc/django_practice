8dfea3bb5f3d25388d51969c24bfbc92fb6f2674
Juanse <jsvargasc@unal.edu.co>
2022-04-24 15:44:13 -0500

    Views and Templates

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!!')
+    latest_question_list = Question.objects.all() # se pueden traer también con filter, etc.
+    return render(request, "polls/index.html", {
+        "latest_question_list" : latest_question_list
+    })
+
+def detail(request, question_id):
+    return HttpResponse(f'Estás viendo la pregunta número {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estás viendo los resultados de la pregunta número {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estás votando a la pregunta número {question_id}')
 
